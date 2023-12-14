2a07f457141b083d449d6f93ce9490a210e2cefa
Birhanu <birhanuasmamaw24@gmail.com>
2023-12-14 08:27:22 -1000

    Editing some features in propertyList

--- src/components/propertyList/propertyList.jsx
+++ src/components/propertyList/propertyList.jsx
@@ -26,7 +26,7 @@ const PropertyList = () => {
             
                 <div className="pListTitles">
                     <h1>Villas</h1>
-                    <h2>125 hotels</h2>
+                    <h2>135hotels</h2>
                 </div>
             </div>
 
