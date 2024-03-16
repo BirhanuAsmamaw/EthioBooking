705ed3f98fc9f39a7e99fcb7fd1be273731d0c33
Birhanu <birhanuasmamaw24@gmail.com>
2024-03-15 22:54:37 -0900

    adding login structure

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,7 +2,8 @@
 import "./login.css";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-
+// import {faUser } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { Container } from './styles';
 
 const Login = () => {
@@ -10,10 +11,26 @@ const Login = () => {
   return (
     <div className="wrapper">
       <form action="">
-        <h1>Login</h1>
+        <h1>Sign In</h1>
         <div className="input-box">
-            <input type="text" />
+            <input type="text"  placeholder="Username" />
+            {/* <FontAwesomeIcon icon={faUser} /> */}
         </div>
+        <div>
+            <input type="password" placeholder="Password"/>
+        </div>
+
+        <div>
+            <label htmlFor=""><input type="checkbox" />Remember me</label>
+            <a href="#">Forgot password?</a>
+        </div>
+
+        <button type="submit">Sign in</button>
+
+        <div>
+            <p>Don't have an account</p>
+        </div>
+
       </form>
     
 
