d0afb85c04590a0144109977dab427f490e950fe
Birhanu <birhanuasmamaw24@gmail.com>
2024-03-15 22:26:50 -0900

    adding login page in to the project

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,45 @@
+
+import "./login.css";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+
+// import { Container } from './styles';
+
+const Login = () => {
+
+  return (
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+            <input type="text" />
+        </div>
+      </form>
+    
+
+
+   {/* <Navbar/> */}
+ 
+   <Footer />
+
+    </div>
+    
+  
+  )
+  
+}
+
+export default Login;
+
+
+{/* Log in
+
+EMAIL IS REQUIRED
+
+PASSWORD
+
+Remember me
+
+Forgot password?
+
+Open account */}
