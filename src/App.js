f6a86bb23b574e8510bda2d524e75d654ed4f119
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-16 22:07:02 -0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./route/route";
 
-
 function App() {
   return (
     <RouterProvider router={router} />
   );
 }
-
 export default App;
