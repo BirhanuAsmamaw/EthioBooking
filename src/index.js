7e9d826ab5551a8504899baaac8e6a97d3bc86d0
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-17 22:32:49 -0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './route/route';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
