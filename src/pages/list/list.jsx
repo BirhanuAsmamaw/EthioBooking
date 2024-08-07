4a09d244011a81d9ade30090601f2b239322aacf
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-07 12:52:00 -0900

    Update list.jsx

--- src/pages/list/list.jsx
+++ src/pages/list/list.jsx
@@ -26,8 +26,7 @@ const List = () => {
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state?.options);
 
-  return (
-    
+  return ( 
     <div>
       {/* <Navbar/> */}
       <Header type={"list"}/>
