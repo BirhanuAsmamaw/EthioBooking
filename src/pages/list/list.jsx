ed41fd6423aed98ced0b9a0316ac1dae12e1ee7e
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-12 11:53:22 -0900

    Update list.jsx

--- src/pages/list/list.jsx
+++ src/pages/list/list.jsx
@@ -19,7 +19,6 @@ const hotels = [
   { id: 8, image: "Hawassa.jpg" }
 ];
 
-
 const List = () => {
   const location = useLocation()
   const [destination, setDestination] = useState(location.state?.destination);
@@ -114,4 +113,5 @@ const List = () => {
     </div>
   )
 }
+
 export  default List;
