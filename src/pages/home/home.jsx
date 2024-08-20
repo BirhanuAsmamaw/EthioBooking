3d1e49b077efc249e8f9f3dbe50a4e43d980d0f9
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-20 09:21:41 -0900

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,10 +9,10 @@ import "./home.css"
 
 const Home = () => {
   return (
-    
     <div>
       <Header/>
       <div className="homeContainer">
+        
         <Feature/>
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
@@ -20,6 +20,7 @@ const Home = () => {
         <FeatureProperties/>
         <MailList/>
         <Footer/>
+        
       </div>
       
     </div>
