6b9b2e52480a6db78a78da128463222f44071dc1
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-09-07 23:37:40 -0900

    styling app folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./route/route";
 function App() {
+
   return (
     <RouterProvider router={router} />
   );
+  
 }
 
 export default App;
--- src/route/route.js
+++ src/route/route.js
@@ -11,12 +11,14 @@ import Login from "../pages/login/login"
   // You can do this:
  export const router = createBrowserRouter(
     createRoutesFromElements(
+
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home/>}/>
         <Route path="hotels" element={<List/>}/>
         <Route path="login" element={<Login/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
       </Route>
+      
     )
   );
 
