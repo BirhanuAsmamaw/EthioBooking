222ad1c2976c1d413436ed5a0e331ca8a9273dfd
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-10-07 01:10:08 -0900

    Update layout.js

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 
 const Layout = () => {
-    return (   
+    return (  
+        
       <div>
         <Navbar/>
         <Outlet/>
       </div>
+    
     );
   };
   export default  Layout;
