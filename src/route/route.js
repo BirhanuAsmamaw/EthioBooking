eb5d23834fae954a43dc779f8e833af571138a77
BirhanuAsmamaw <birhanuasmamaw24@gmail.com>
2024-10-16 02:39:23 +0300

    cleaning  codes

--- src/route/route.js
+++ src/route/route.js
@@ -9,6 +9,7 @@ import Layout from "../layout/layout";
 import List from "../pages/list/list";
 import Login from "../pages/login/login"
 
+
   // You can do this:
  export const router = createBrowserRouter(
     createRoutesFromElements(
